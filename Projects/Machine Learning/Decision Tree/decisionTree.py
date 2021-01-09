d12efd0af952ac204e1d2af0261b79bcdfc02301
glosophy <suttersg@gmail.com>
2021-01-08 20:23:13 -0500

    Update

--- Projects/Machine Learning/Decision Tree/decisionTree.py
+++ Projects/Machine Learning/Decision Tree/decisionTree.py
@@ -15,6 +15,7 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # get cwd
@@ -54,51 +55,50 @@ y_pred_en = clf_entropy.predict(X_test)
 
 # print accuracies
 print("Accuracy for Gini tree is ", round((accuracy_score(y_test, y_pred) * 100), 2))
-print("Accuracy for Entropy tree is ", round((accuracy_score(y_test, y_pred_en)*100), 2))
+print("Accuracy for Entropy tree is ", round((accuracy_score(y_test, y_pred_en) * 100), 2))
 
 # classification metrics
 print("\n")
 print("Results using Gini Index:")
 print("Classification Report: ")
-print(classification_report(y_test,y_pred))
+print(classification_report(y_test, y_pred))
 print("\n")
 
 print("Results using Entropy:")
 print("Classification Report: ")
-print(classification_report(y_test,y_pred_en))
+print(classification_report(y_test, y_pred_en))
 print("\n")
-
 
 # confusion matrix for gini model
 conf_matrix = confusion_matrix(y_test, y_pred)
 class_names = df['class'].unique()
 df_cm = pd.DataFrame(conf_matrix, index=class_names, columns=class_names)
 
-plt.figure(figsize=(3,3))
-hm = sns.heatmap(df_cm, cbar=False, annot=True, square=True, fmt='d', annot_kws={'size': 14}, yticklabels=df_cm.columns, xticklabels=df_cm.columns)
+plt.figure(figsize=(3, 3))
+hm = sns.heatmap(df_cm, cbar=False, annot=True, square=True, fmt='d', annot_kws={'size': 14}, yticklabels=df_cm.columns,
+                 xticklabels=df_cm.columns)
 hm.yaxis.set_ticklabels(hm.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
 hm.xaxis.set_ticklabels(hm.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
-plt.ylabel('True label',fontsize=14)
-plt.xlabel('Predicted label',fontsize=14)
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
 plt.title('Confusion Matrix Gini Model')
 plt.tight_layout()
 plt.show()
-
 
 # confusion matrix for entropy model
 conf_matrix_en = confusion_matrix(y_test, y_pred_en)
 df_cm_en = pd.DataFrame(conf_matrix_en, index=class_names, columns=class_names)
 
-plt.figure(figsize=(3,3))
-hm = sns.heatmap(df_cm_en, cbar=False, annot=True, square=True, fmt='d', annot_kws={'size': 14}, yticklabels=df_cm_en.columns, xticklabels=df_cm_en.columns)
+plt.figure(figsize=(3, 3))
+hm = sns.heatmap(df_cm_en, cbar=False, annot=True, square=True, fmt='d', annot_kws={'size': 14},
+                 yticklabels=df_cm_en.columns, xticklabels=df_cm_en.columns)
 hm.yaxis.set_ticklabels(hm.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
 hm.xaxis.set_ticklabels(hm.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
-plt.ylabel('True label',fontsize=14)
-plt.xlabel('Predicted label',fontsize=14)
+plt.ylabel('True label', fontsize=14)
+plt.xlabel('Predicted label', fontsize=14)
 plt.title('Confusion Matrix Entropy Model')
 plt.tight_layout()
 plt.show()
-
 
 # graph decision tree
 cols = X.columns
@@ -115,7 +115,6 @@ graph_entropy.write_pdf("TreeEntropy.pdf")
 webbrowser.open_new(r'TreeGini.pdf')
 webbrowser.open_new(r'TreeEntropy.pdf')
 
-
 # get more insights by playing around with DecisionTreeClassifier( ) arguments
 max_depth = [3, 6, 9, 12, 15]
 min_samples_leaf = [5, 10, 15, 20, 25]
@@ -124,16 +123,17 @@ cols = ['max_depth', 'min_samples_leaf', 'acc_gini', 'acc_entropy', 'auc_gini', 
 rows = len(max_depth) * len(min_samples_leaf)
 df2 = pd.DataFrame(columns=cols, index=range(rows))
 
-k = 0 # initialize counter
+k = 0  # initialize counter
 for i in range(len(max_depth)):
     for j in range(len(min_samples_leaf)):
-
         # define
         clf_gini = DecisionTreeClassifier(criterion="gini", random_state=42,
-                                          max_depth=max_depth[i], min_samples_leaf=min_samples_leaf[j], class_weight='balanced')
+                                          max_depth=max_depth[i], min_samples_leaf=min_samples_leaf[j],
+                                          class_weight='balanced')
 
         clf_entropy = DecisionTreeClassifier(criterion="entropy", random_state=42,
-                                             max_depth=max_depth[i], min_samples_leaf=min_samples_leaf[j], class_weight='balanced')
+                                             max_depth=max_depth[i], min_samples_leaf=min_samples_leaf[j],
+                                             class_weight='balanced')
 
         # fit
         clf_gini.fit(X_train_std, y_train)
@@ -160,7 +160,6 @@ for i in range(len(max_depth)):
         df2.loc[k].acc_entropy = acc_entropy
         df2.loc[k].auc_gini = roc_auc
         df2.loc[k].auc_entropy = roc_auc_en
-        k+=1
-
+        k += 1
 
 print(df2)
