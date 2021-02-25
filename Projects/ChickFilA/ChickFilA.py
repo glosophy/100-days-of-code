52ef2c1c5f1aa5c6b9801b10893dcbeb00a61abc
glosophy <suttersg@gmail.com>
2021-02-24 21:45:57 -0400

    Update

--- Projects/ChickFilA/ChickFilA.py
+++ Projects/ChickFilA/ChickFilA.py
@@ -12,48 +12,50 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
 
-    html.Img(id='image', src="https://cdn.clipart.email/49ee1e3c8f46800efe4d7e2f01658681_transparent-chick-fil-a-clipart_317-317.png",
-                style = {'position': 'relative',
-                         'margin': 'auto',
-                         'display':'block',
-                         'margin-top':'70px'
-                         }),
+    html.Img(id='image',
+             src="https://cdn.clipart.email/49ee1e3c8f46800efe4d7e2f01658681_transparent-chick-fil-a-clipart_317-317.png",
+             style={'position': 'relative',
+                    'margin': 'auto',
+                    'display': 'block',
+                    'margin-top': '70px'
+                    }),
 
-             html.Div([
-                 html.Button('Is Chick-Fil-A open?', id='btn-nclicks-1', n_clicks=0,
+    html.Div([
+        html.Button('Is Chick-Fil-A open?', id='btn-nclicks-1', n_clicks=0,
+                    style={
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin-top': '50px',
+                        'margin-left': '38%'
+                    }),
+
+        html.H1(id='container-button-timestamp',
                 style={
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderRadius': '5px',
-                    'textAlign': 'center',
-                    'margin-top': '50px',
-                    'margin-left':'38%'
+                    'position': 'relative',
+                    'font-weight': 'bold',
+                    'margin-left': '44%',
+                    'margin-top': '50px'
                 }),
 
-    html.H1(id='container-button-timestamp',
-             style={
-                 'position':'relative',
-                 'font-weight':'bold',
-                 'margin-left':'44%',
-                 'margin-top':'50px'
-                 }),
-
-    dcc.Markdown('''
+        dcc.Markdown('''
                 Brought to you by GSS ♥.
                 ''',
 
-                 style={'position':'relative',
-                        'textAlign': 'center',
-                        'margin-top': '300px'
-                        })
+                     style={'position': 'relative',
+                            'textAlign': 'center',
+                            'margin-top': '300px'
+                            })
 
     ])
 
 ], style={'margin': 'auto',
           'display': 'block',
-          'width':'50%'
-    })
+          'width': '50%'
+          })
+
 
 @app.callback(Output('container-button-timestamp', 'children'),
               Input('btn-nclicks-1', 'n_clicks'))
@@ -70,6 +72,7 @@ def displayClick(btn1):
             result = 'YES!'
 
     return html.Div(result)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
