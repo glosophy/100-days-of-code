5e57169d4cccd0da7f742deffb7c41933d4a9231
glosophy <suttersg@gmail.com>
2021-03-07 23:44:42 -0400

    Update

--- Projects/Interview Questions/ConfettiAI/linear_interpolation.py
+++ Projects/Interview Questions/ConfettiAI/linear_interpolation.py
@@ -5,6 +5,16 @@ Then evaluate your linear regression fit on the provided point.
 """
 import numpy as np
 
+def gradient_descent(X, y, weights, learning_rate, n_iters):
+    n_samples = len(y)
+    # NOTE: A better strategy is to compute the cost at each step and run until
+    # the cost converges to a small theshold value
+    for i in range(n_iters):
+        weights = weights - learning_rate * np.dot(X.T, np.dot(X, weights) - y.reshape(-1, 1))
+
+    return weights
+
+
 def linear_interpolate_point(data, test_points):
     """
     Fit a line to the provided data and then evaluate on the provided test point.
@@ -13,9 +23,16 @@ def linear_interpolate_point(data, test_points):
     :return: The output of your point on the interpolated line
     """
     # fill in the function here
-    pass
+    learning_rate = 0.01
+    n_iters = 2000
+    weights = np.zeros(2).reshape(-1, 1)
+    data = np.array(data)
+    X = np.ones_like(data)
+    X[:, 0] = data[:, 0]
+    weights = gradient_descent(X, data[:, 1], weights, learning_rate, n_iters)
+    return test_points * weights[0, 0] + weights[1, 0]
+
 
 linear_interpolate_point(
     [(1, 2), (3,4), (5,6), (8, 8), (7, -1)],
-    10.5
-)
+    10.5)
